b4a087b731a4b1f8513b8342c10c30c1799efc96
meawpanipa <73675653+meawpanipa@users.noreply.github.com>
2020-10-30 17:29:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
     <p>ชื่อเล่น มะเหมี่ยว</p>
     <p>รหัสนักศึกษา 63070130</p>
     <br>
-        <a href="https://www.youtube.com/watch?v=QxZB1H-SiWU">มาย้อนวัยกันหน่อย จิ้มเลย</a>
+        <a href="https://www.youtube.com/watch?v=QxZB1H-SiWU">มาย้อนวัยกันหน่อย จิ้มเลย</a> <img src="cat.png">
     
     <?php
-        echo "<h1>วันนี้มีเรื่องตลกจะเล่าให้ฟัง</h1>";
-        echo "<h1></h1>";
+        echo "<h2>วันนี้มีเรื่องตลกจะเล่าให้ฟัง</h2>";
+        echo "<h3>เรื่องมันมีอยู่ว่า.........</h3>";
+        echo "<h4>........</h4>";
     ?>
+    <img src="ch.gif">
 </body>
