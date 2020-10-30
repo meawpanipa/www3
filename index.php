b5b883e1e4cb8c807c9ccd8cfd934b1bd542fa2c
meawpanipa <73675653+meawpanipa@users.noreply.github.com>
2020-10-30 17:38:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
     <title>LAB ITF-11</title>
 </head>
 <body>
-    <BODY BGCOLOR="#FF0000"> 
+    <BODY BGCOLOR="#F0E68C">
+    <FONT FACE="AngsanaUPC">
     <h1><font color="blue">รับไปเลย ผลงานแลปประจำวันนี้</font></h1>
     <p>นางสาว ภรณีนิภา สองเมือง</p>
     <p>ชื่อเล่น มะเหมี่ยว</p>
     <p>รหัสนักศึกษา 63070130</p>
+    </FONT>
     <br>
         <a href="https://www.youtube.com/watch?v=QxZB1H-SiWU">มาย้อนวัยกันหน่อย จิ้มเลย</a> <img src="cat.png">
     
